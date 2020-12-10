cf17d7c6d4063e7573badc21ff2e65fa28d14a12
MaksymHurianov <11maxim11@ua.fm>
2020-12-10 19:02:55 +0300

    added storybook

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -30,7 +30,7 @@ test('correct task should be deleted from correct array', () => {
 test('correct task should be added to correct array', () => {
     const action = addTaskAC("juce", "todolistId2");
 
-    const endState = tasksReducer(startState, action)git
+    const endState = tasksReducer(startState, action)
 
     expect(endState["todolistId1"].length).toBe(3);
     expect(endState["todolistId2"].length).toBe(4);
